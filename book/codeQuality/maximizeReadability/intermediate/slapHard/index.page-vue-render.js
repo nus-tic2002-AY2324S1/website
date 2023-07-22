fe0c5e8a9b8e120c4a53e33c5ed97ce66d7ac6a9
damithc <damith@github>
2023-07-22 19:08:44 +0800

    Site Update. [skip ci]

--- book/codeQuality/maximizeReadability/intermediate/slapHard/index.page-vue-render.js
+++ book/codeQuality/maximizeReadability/intermediate/slapHard/index.page-vue-render.js
@@ -26,6 +26,6 @@ with(this){return _c('strong',[_c('em',[_v("Single Level of Abstraction Principl
 with(this){return _c('span',{staticClass:"dimmed"},[_v("while the book "),_c('em',[_v("Clean Code")]),_v(" (by Robert C. Martin) calls this "),_c('em',[_v("One Level of Abstraction per Function")])])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.1")]),_v(", generated on Sat, 22 Jul 2023, 18:50:18 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.1")]),_v(", generated on Sat, 22 Jul 2023, 19:05:09 GMT+8]")])])])}
 }];
   
--- se-book-adapted/chapters-printable/modeling-printable.page-vue-render.js
+++ se-book-adapted/chapters-printable/modeling-printable.page-vue-render.js
@@ -107,6 +107,6 @@ with(this){return _c('div',[_c('h2',{attrs:{"id":"modeling-behaviors"}},[_c('spa
 with(this){return _c('p',[_c('strong',[_v("Sequence diagrams model the interactions between various entities in a system, in a specific scenario.")]),_v(" Modelling such scenarios is useful, for example, to verify the design of the internal interactions is able to provide the expected outcomes.")])}
 },function anonymous(
 ) {
-with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.1")]),_v(", generated on Sat, 22 Jul 2023, 18:50:18 GMT+8]")])])])}
+with(this){return _c('footer',[_c('div',{staticClass:"text-center"},[_c('small',[_v("["),_c('span',[_c('strong',[_v("Powered by")])]),_v(" "),_c('img',{attrs:{"src":"https://markbind.org/favicon.ico","width":"30"}}),_v(" "),_c('a',{attrs:{"href":"https://markbind.org/"}},[_v("MarkBind 5.0.1")]),_v(", generated on Sat, 22 Jul 2023, 19:05:09 GMT+8]")])])])}
 }];
   
